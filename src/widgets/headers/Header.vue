09418038b507ca0716512dd9223dd207aee98bce
erastian <erastian@gmail.com>
2024-06-08 18:39:19 +0200

    Work: Header fixes
    - added separated StickHeader component
    - change onScroll behavior to gsap.ScrollTrigger
    - minor fixes

--- src/widgets/headers/Header.vue
+++ src/widgets/headers/Header.vue
@@ -5,7 +5,6 @@ import Menu from "@/entities/header/Menu.vue";
 import Logo from "@/entities/header/Logo.vue";
 
 withDefaults(defineProps<{
-  showSticky: boolean,
   showMenu?: boolean
 }>(), {
   showMenu: true
@@ -13,19 +12,15 @@ withDefaults(defineProps<{
 </script>
 
 <template>
-  <header class="header" :class="{sticky: showSticky}">
+  <header class="header">
     <div class="header-container">
       <div class="menuHolder">
-        <RouterLink v-show="showSticky" to="/" class="smallLogo" >
-          <glitched-writer class="firstChild" text="ERASTIAN" appear/>
-          <glitched-writer class="secondChild" text="the FrontEnd dev" appear/>
-        </RouterLink>
-        <RouterLink v-show="!showSticky" to="/">
+        <RouterLink to="/">
           <Logo/>
         </RouterLink>
-        <Menu v-if="showMenu" :show-sticky="showSticky"/>
+        <Menu v-if="showMenu"/>
       </div>
-      <div v-show="!showSticky" class="logoNameHolder">
+      <div class="logoNameHolder">
         <UserPic/>
         <div class="name">
           <glitched-writer text="m." appear/>
@@ -61,90 +56,99 @@ withDefaults(defineProps<{
   width: 100%;
   transition: 0.3s ease;
   border-radius: 0 0 .5rem .5rem;
+
+
+  &:after {
+    position: absolute;
+    content: "";
+    bottom: 1.5rem;
+    left: 0;
+    right: 0;
+    z-index: -1;
+    height: 10%;
+    width: 100%;
+    margin: 0 auto;
+    transform: scale(0.95);
+    -webkit-filter: blur(1.7rem);
+    -moz-filter: blur(1.7rem);
+    -ms-filter: blur(1.7rem);
+    filter: blur(1.7rem);
+    background: linear-gradient(270deg, #0fffc1, #7e0fff);
+    background-size: 200% 200%;
+    animation: animateGlow 5s ease infinite;
+  }
 }
 
-.header-container:after {
-  position: absolute;
-  content: "";
-  bottom: 1.5rem;
-  left: 0;
-  right: 0;
-  z-index: -1;
-  height: 10%;
-  width: 100%;
-  margin: 0 auto;
-  transform: scale(0.95);
-  -webkit-filter: blur(1.7rem);
-  -moz-filter: blur(1.7rem);
-  -ms-filter: blur(1.7rem);
-  filter: blur(1.7rem);
-  background: linear-gradient(270deg, #0fffc1, #7e0fff);
-  background-size: 200% 200%;
-  animation: animateGlow 5s ease infinite;
-}
-@keyframes animateGlow {
-  0%{background-position:0 50%}
-  50%{background-position:100% 50%}
-  100%{background-position:0 50%}
-}
 .menuHolder {
   display: flex;
   justify-content: space-between;
   width: 100%;
+
+  a {
+    text-decoration: none;
+    margin: auto 0;
+  }
+
+  .smallLogo .firstChild {
+    text-decoration: none;
+    color: var(--color-white);
+    font-family: var(--font-menu);
+    font-size: 3rem;
+    margin: auto 0;
+  }
+
+  .smallLogo .secondChild {
+    font-size: 1rem;
+    text-decoration: none;
+    color: var(--color-white);
+    font-family: var(--font-menu);
+    margin: auto 0 auto .5rem;
+  }
 }
 
-.menuHolder a {
-  text-decoration: none;
-  margin: auto 0;
-}
-
-.menuHolder .smallLogo .firstChild {
-  text-decoration: none;
-  color: var(--color-white);
-  font-family: var(--font-menu);
-  font-size: 3rem;
-  margin: auto 0;
-}
-
-.menuHolder .smallLogo .secondChild {
-  font-size: 1rem;
-  text-decoration: none;
-  color: var(--color-white);
-  font-family: var(--font-menu);
-  margin: auto 0 auto .5rem;
-}
 
 .logoNameHolder {
   display: flex;
   justify-content: space-between;
   -webkit-min-logical-height: 17rem;
   min-height: 17rem;
+
+  .name {
+    display: flex;
+    flex-direction: column;
+    justify-content: end;
+    margin-top: auto;
+    font-family: var(--font-menu);
+    color: var(--color-white);
+    font-size: 6rem;
+    font-weight: 100;
+    line-height: 4.5rem;
+
+    span {
+      margin-top: 1rem;
+      margin-left: auto;
+      letter-spacing: 0.25rem;
+    }
+
+    span:first-child {
+      margin-right: -1.45rem;
+    }
+
+    span:last-child {
+      margin-right: -0.5rem;
+    }
+  }
 }
 
-.name {
-  display: flex;
-  flex-direction: column;
-  justify-content: end;
-  margin-top: auto;
-  font-family: var(--font-menu);
-  color: var(--color-white);
-  font-size: 6rem;
-  font-weight: 100;
-  line-height: 4.5rem;
+@keyframes animateGlow {
+  0% {
+    background-position: 0 50%
+  }
+  50% {
+    background-position: 100% 50%
+  }
+  100% {
+    background-position: 0 50%
+  }
 }
-
-.name span {
-  margin-top: 1rem;
-  margin-left: auto;
-  letter-spacing: 0.25rem;
-}
-
-.name span:first-child {
-  margin-right: -1.45rem;
-}
-
-.name span:last-child {
-  margin-right: -0.5rem;
-}
-
 </style>
